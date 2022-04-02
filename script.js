42ea9ff2a2f22781b5729d70c799b7634a18c9a6
Dmitry Gorbach <89874530087@mail.ru>
2022-04-02 13:09:38 +0200

    fix lesson06

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function game() {
-  const num = 238;
+  const num = Math.floor(Math.random() * (101 - 1) + 1);
+  console.log(num);
   let count = prompt("Угадай число от 1 до 100");
 
   const check = function (countNum) {
     if (count !== null) {
       if (countNum > num) {
-        count = prompt("Загаданное число больше, попробуй снова");
+        count = prompt("Загаданное число меньше, попробуй снова");
         check(count);
       } else if (countNum < num) {
-        count = prompt("Загаданное число меньше, попробуй снова");
+        count = prompt("Загаданное число больше, попробуй снова");
         check(count);
       } else if (countNum == num) {
         alert("Вы угадали!");
